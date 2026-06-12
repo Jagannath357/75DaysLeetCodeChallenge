390a8d1fc93b43d71cfc6b5b7b8c44e9e8e05eba
Jagannath Padhi <padhijaga200@gmail.com>
2026-06-12 09:38:47 +0530

    Time: 2 ms (96.76%), Space: 48.6 MB (67.2%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,4 @@
 class Solution {
-    // public String getBinary(int n){
-    //     return Integer.toBinaryString(n);
-    // }
-    // public int getTotalSet(int n){
-    //     if(n == 0) return 0;
-    //     int count = 0;
-    //     while(n != 0){
-    //         count++;
-    //         n &= (n-1);
-    //     }
-    //     return count;
-    // }
     public int[] countBits(int n) {
         int i;
         int arr[] = new int[n+1];
